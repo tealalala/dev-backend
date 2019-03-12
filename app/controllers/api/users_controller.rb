be8a330ddff900c52d8dd2741bc264ec8a767919
josephpcoyne <josephpcoyne@gmail.com>
2019-03-12 16:14:28 -0400

    adds user create action

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,17 @@
 class Api::UsersController < ApplicationController
+  def create
+    user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+
+    if user.save
+      render json: {message: 'User created successfully'}, status: :created
+    else
+      render json: {errors: user.errors.full_messages}, status: :bad_request
+    end
+  end
 end
