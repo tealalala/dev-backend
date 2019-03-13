c1d52d029649ecf7acae193c54283ca3ad7ff2c6
Tea <tealalala@protonmail.com>
2019-03-13 14:31:50 -0700

    setup baseline posts controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,25 @@
 class Api::PostsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   namespace :api do
+    # Posts
+    resources :posts
+
+    # Users
     get "/users/me" => "users#profile"
     get "/users/:id" => "users#show"
     post "/users" => "users#create"
     patch "/users/:id" => "users#update"
     delete "/users/:id" => "users#destroy"
-    
-    
+
+    # Sessions
     post "/sessions" => "sessions#create"
   end
 end
