d61bcf6b6969320af3f452cdd82028fa3c96da1f
Tea <tealalala@protonmail.com>
2019-03-19 13:18:29 -0700

    add model associations: posts / join table: post_user_votes / users

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  # Model Relationships
+  has_many :post_user_votes
+  has_one :user, through: :post_user_votes
+        # it makes sense to have has_one :user (1 post created by 1 user) but maybe we need has_many :users to make this a proper join table
 end
--- app/models/post_tag.rb
+++ app/models/post_tag.rb
@@ -1,2 +1,5 @@
 class PostTag < ApplicationRecord
+  # Active Record Association: posts / JOIN: post_user_votes / users
+  belongs_to :post
+  belongs_to :user
 end
