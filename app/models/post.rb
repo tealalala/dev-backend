ea348eff87511891952a132b51b43e46c11c0d9e
Tea <tealalala@protonmail.com>
2019-03-19 13:20:06 -0700

    add model associations: posts / join table: post_tags / tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
-  # Model Relationships
+  # Active Record Association: posts / JOIN: post_tags / tags
+  has_many :post_tags
+  has_many :tags, through: :post_tags
+
+  # Active Record Association: posts / JOIN: post_user_votes / users
   has_many :post_user_votes
   has_one :user, through: :post_user_votes
         # it makes sense to have has_one :user (1 post created by 1 user) but maybe we need has_many :users to make this a proper join table
--- app/models/post_tag.rb
+++ app/models/post_tag.rb
@@ -1,4 +1,8 @@
 class PostTag < ApplicationRecord
+  # Active Record Association: posts / JOIN: post_tags / tags
+  belongs_to :post
+  belongs_to :tag
+
   # Active Record Association: posts / JOIN: post_user_votes / users
   belongs_to :post
   belongs_to :user
