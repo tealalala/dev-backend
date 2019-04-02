7987a2ac9b04e111390357bc1d001e967dbdca5a
Tea <tealalala@protonmail.com>
2019-04-02 16:21:38 -0700

    set local_env.yml environment var in config > application.rb

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,15 @@ require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+# Set environment variables from local_env.yml file
+# Source: https://qiita.com/alokrawat050/items/0d7791b3915579f95791
+config.before_configuration do
+  env_file = File.join(Rails.root, 'config', 'local_env.yml')
+  YAML.load(File.open(env_file)).each do |key, value|
+    ENV[key.to_s] = value
+  end if File.exists?(env_file)
+end
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
